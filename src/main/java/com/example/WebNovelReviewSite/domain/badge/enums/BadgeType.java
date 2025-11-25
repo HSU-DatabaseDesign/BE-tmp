fc5a9992fe20e2e14f27265723886dbcbcf84f95
mjw207 <wodnjs3961@naver.com>
2025-11-26 00:46:41 +0900

    뱃지기능update

---
+++ src/main/java/com/example/WebNovelReviewSite/domain/badge/enums/BadgeType.java
@@ -0,0 +1,10 @@
+package com.example.WebNovelReviewSite.domain.badge.enums;
+
+public enum BadgeType {
+    REVIEW_COUNT,      // 리뷰 작성 개수
+    COLLECTION_COUNT,  // 컬렉션 생성 개수
+    FOLLOW_COUNT,      // 팔로우 개수
+    LIKE_COUNT,        // 좋아요 받은 개수
+    LOGIN_DAYS         // 로그인 일수
+}
+
